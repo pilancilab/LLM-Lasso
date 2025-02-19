b4de8b65695e3837f3e650db1242b09789a66bcc
Naomi Sagan <naomi.sagan@berkeley.edu>
2025-02-19 15:24:12 -0800

    Add LLM-Score CLI

--- scripts/llm_score.py
+++ scripts/llm_score.py
@@ -4,8 +4,8 @@ sys.path.append('.')
 import warnings
 import constants
 import os
-from langchain_openai import OpenAIEmbeddings
 from llm_lasso.llm_penalty.penalty_collection import collect_penalties, PenaltyCollectionParams
+from llm_lasso.baselines.llm_score import llm_score
 from transformers.hf_argparser import HfArgumentParser
 from dataclasses import dataclass, field
 from langchain_community.vectorstores import Chroma
@@ -27,13 +27,34 @@ class Arguments:
     save_dir: str = field(metadata={
         "help": "Directory to save the results and scores."
     })
+    
+    batch_size: int = field(default=30, metadata={
+        "help": "Number of genes to pass into the LLM at once"})
+    n_trials: int = field(default=1, metadata={
+        "help": "Number of trials to average over"})
+    wipe: bool = field(default=False, metadata={
+        "help": "Wipe the save directory before starting"})
+    
+    k_min: int = field(default=0, metadata={
+        "help": "Minimum number of top genes to select."
+    })
+    k_max = field(default=50, metadata={
+        "help": "Maximum number of top genes to select."
+    })
+    step = field(default=5, metadata={
+        "help": "Step size for the range of k values."
+    })
+
+    def clamp_kmax_and_step(self, n_features):
+        self.k_max = min(self.k_max, n_features)
+        self.step = min(self.step, self.k_max - self.k_min - 1)
 
 
 if __name__ == "__main__":
     os.environ["OPENAI_API_KEY"] = constants.OPENAI_API
 
-    parser = HfArgumentParser([PenaltyCollectionParams, Arguments, LLMParams])
-    (penalty_params, args, llm_params) = parser.parse_args_into_dataclasses()
+    parser = HfArgumentParser([Arguments, LLMParams])
+    (args, llm_params) = parser.parse_args_into_dataclasses()
 
     # Load gene names
     if args.feature_names_path.endswith(".pkl"):
@@ -44,31 +65,25 @@ if __name__ == "__main__":
             feature_names = file.read().splitlines()
     else:
         raise ValueError("Unsupported file format. Use .pkl or .txt.")
+    args.clamp_kmax_and_step(len(feature_names))
     
     print(f'Total number of features in processing: {len(feature_names)}.')
 
     # Initialize LLM
     model = llm_params.get_model()
 
-    # Initialize embeddings and vector store
-    embeddings = OpenAIEmbeddings()
-    if os.path.exists(constants.OMIM_PERSIST_DIRECTORY):
-        vectorstore = Chroma(persist_directory=constants.OMIM_PERSIST_DIRECTORY, embedding_function=embeddings)
-    else:
-        raise FileNotFoundError(f"Vector store not found at {constants.OMIM_PERSIST_DIRECTORY}. Ensure data is preprocessed and saved.")
-
-    results, all_scores = collect_penalties(
+    top_k_dict = llm_score(
         category=args.category,
         feature_names=feature_names,
-        prompt_file=args.prompt_filename,
+        prompt_filename=args.prompt_filename,
         save_dir=args.save_dir,
-        vectorstore=vectorstore,
         model=model,
-        params=penalty_params,
-        omim_api_key=constants.OMIM_KEYS[0]
+        batch_size=args.batch_size,
+        n_trials=args.n_trials,
+        wipe=args.wipe,
+        k_min=args.k_main,
+        k_max=args.k_max,
+        step=args.step
     )
-    print(f'Total number of scores collected: {len(all_scores)}.')
 
-    if len(all_scores) != len(feature_names):
-        raise ValueError(
-            f"Mismatch between number of scores ({len(all_scores)}) and number of gene names ({len(feature_names)}).")
+    print("Top-k gene selection complete. Results saved.")
